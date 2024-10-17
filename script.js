46b2d7b6325a57cc4896b5b394840f75ee420bed
Henrique <henriquemenezescg@gmail.com>
2024-10-17 14:14:41 -0300

    Add drawing functionality with color selection and clear screen option

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Initial Data
 let currentColor = 'black';
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
@@ -14,6 +16,8 @@ screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
 
+document.querySelector('.clear').addEventListener('click',clearScreen);
+
 // Functions
 function colorClickEvent(e){
   let color = e.target.getAttribute('data-color');
@@ -23,16 +27,40 @@ function colorClickEvent(e){
   e.target.classList.add('active');
 }
 
-function mouseDownEvent(){
+function mouseDownEvent(e){
   canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
 }
 
-function mouseMoveEvent(){
+function mouseMoveEvent(e){
   if(canDraw){
-    canDraw();
+    draw(e.pageX,e.pageY);
   }
 }
 
 function mouseUpEvent(){
   canDraw = false;
+}
+
+function draw(x,y){
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.lineJoin = "round";
+  ctx.moveTo(mouseX,mouseY);
+  ctx.lineTo(pointX,pointY);
+  ctx.closePath();
+  ctx.strokeStyle = currentColor;
+  ctx.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
+}
+
+function clearScreen(){
+  ctx.setTransform(1,0,0,1,0,0);
+  ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
 }
